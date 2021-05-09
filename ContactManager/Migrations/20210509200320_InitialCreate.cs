dd98f21680bf545fb2f7535b505c725260c825fd
Cathy A. Lue Chee Lip <LueCheCA@PWCS.EDU>
2021-05-09 17:58:35 -0400

    Add project files.

---
+++ ContactManager/Migrations/20210509200320_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactManager.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contact",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MiddleName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AddressStreet = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AddressCity = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AddressState = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AddressZip = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneType = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contact", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contact");
+        }
+    }
+}
---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.Models
+{
+    public enum PhoneTypeEnum
+    {
+        Home = 1,
+        Work = 2,
+        Mobile = 3
+    }
+    public class Contact
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string AddressStreet { get; set; }
+        public string AddressCity { get; set; }
+        public string AddressState { get; set; }
+        public string AddressZip { get; set; }
+        public string PhoneNumber { get; set; }
+        public PhoneTypeEnum PhoneType { get; set; }
+        public string Email { get; set; }
+    }
+}
